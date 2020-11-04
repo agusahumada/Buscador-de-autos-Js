656d5ca1a21d79e10cf734eb348e636dba4436e3
agusahumada <agustinaahumada649@gmail.com>
2020-11-04 16:02:48 -0300

    Filtrando por año

--- js/app.js
+++ js/app.js
@@ -39,7 +39,8 @@ marca.addEventListener("change", (e) => {
 });
 
 year.addEventListener("change", (e) => {
-  datosBusqueda.year = e.target.value;
+  datosBusqueda.year = parseInt(e.target.value);
+  filtrarAuto();
 });
 
 minimo.addEventListener("change", (e) => {
@@ -90,13 +91,23 @@ function llenarSelect() {
 
 //Funcion que filtra en base a la búsqueda
 function filtrarAuto() {
-  const resultado = autos.filter(filtrarMarca);
+  const resultado = autos.filter(filtrarMarca).filter(filtrarYear);
   console.log(resultado);
 }
+
 function filtrarMarca(auto) {
   const { marca } = datosBusqueda;
   if (marca) {
     return auto.marca === datosBusqueda.marca;
+  }
+  return auto;
+}
+
+function filtrarYear(auto) {
+  const { year } = datosBusqueda;
+
+  if (year) {
+    return auto.year === year;
   }
   return auto;
 }
